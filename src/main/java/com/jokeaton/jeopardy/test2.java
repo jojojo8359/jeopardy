63a347c6c3390cf8e5c25e8c86f6503244a2146b
jojojo8359 <elementmania575@gmail.com>
2019-08-27 18:50:40 -0400

    Remove testing class

--- src/main/java/com/jokeaton/jeopardy/test2.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.jokeaton.jeopardy;
-
-public class test2 {
-    public static void main(String[] args) {
-//        String guess = "SAT";
-//        String answer = "the SAT /Scholastic Aptitude Test";
-//        System.out.println(Game.checkAnswer(guess, answer));
-    }
-}
